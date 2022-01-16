320170a5a7e1e76e7033fe7629a9ff7770b25f34
HanaaMohammed96 <hanamohammed622@gmail.com>
2022-01-16 19:08:30 +0200

    fix problem of 204 in realState

--- src/app/@core/api.ts
+++ src/app/@core/api.ts
@@ -2200,7 +2200,7 @@ export class RealStatesClient implements IRealStatesClient {
                 (<any>response).error instanceof Blob ? (<any>response).error : undefined;
 
         let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); } }
-        if (status === 200) {
+        if (status === 204) {
             return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
                 return _observableOf<void>(<any>null);
             }));
@@ -2250,7 +2250,7 @@ export class RealStatesClient implements IRealStatesClient {
                 (<any>response).error instanceof Blob ? (<any>response).error : undefined;
 
         let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); } }
-        if (status === 200) {
+        if (status === 204) {
             return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
                 return _observableOf<void>(<any>null);
             }));
